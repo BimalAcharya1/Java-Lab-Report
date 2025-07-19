d7b37e1d936903beefddbc0d35da267716914235
Bimal Acharya <177016396+BimalAcharya1@users.noreply.github.com>
2025-07-19 15:37:41 +0545

    Update SynchronizationEx.java

--- lab8/SynchronizationEx.java
+++ lab8/SynchronizationEx.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+/* 6.Write a java program to demonstrate the concept of synchronization in multithreaded applications. */
 package Lab8;
 
 class print {
