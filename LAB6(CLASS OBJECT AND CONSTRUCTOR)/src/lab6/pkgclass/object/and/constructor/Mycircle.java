757a79888c2496422503149e7d4d0e0bd2ab96e6
Bimal Acharya <177016396+BimalAcharya1@users.noreply.github.com>
2025-07-19 14:56:17 +0545

    Update Mycircle.java

--- LAB6(CLASS OBJECT AND CONSTRUCTOR)/src/lab6/pkgclass/object/and/constructor/Mycircle.java
+++ LAB6(CLASS OBJECT AND CONSTRUCTOR)/src/lab6/pkgclass/object/and/constructor/Mycircle.java
@@ -1,3 +1,8 @@
+/* 1. Write a class Circle containing private variable radius of type float, suitable 
+constructors, and two methods findArea and findCricumference to find area 
+and circumference of circles respectively. Write a separate class MyCircle 
+containing main method to create and use circle objects. */
+
 class circle {
     float pi;
     private float radius;
