6e86c1ee4e45b6e8e7f606441d3558a266dccfaf
Bimal Acharya <177016396+BimalAcharya1@users.noreply.github.com>
2025-07-19 14:16:04 +0545

    Update question1.java

--- LAB2(JAVA DATA TYPES , EXPRESSIONS & OPERETORS)/question1.java
+++ LAB2(JAVA DATA TYPES , EXPRESSIONS & OPERETORS)/question1.java
@@ -1,3 +1,8 @@
+//1. Write a java program to evaluate following expression if a=    9, b=13 and c=3.
+  i.   x= a-b/3.0+c*2-1;
+  ii.  y= a-(float)b/(3+c)*(2-1);
+  iii. z= a-((float)b/(3+c)*2)-1;
+//
 public class question1{
 public static void main(String[]args){
 int a=9;
